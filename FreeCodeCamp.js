b0344fe4b19470fc6bfc09d9550e446746bc6d0e
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-08-05 22:16:42 -0300

    2-Criando um aplicativo de boletim de notas

--- FreeCodeCamp.js
+++ FreeCodeCamp.js
@@ -31,3 +31,47 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+// ---------------------------------------------------------
+console.log("\n Criando um aplicativo de boletim de notas \n");
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+function studentMsg(totalScores, studentScore) {
+  let media = getAverage(totalScores);
+  let nota = getGrade(studentScore);
+  if(nota === "F"){
+    return "Class average: "+ media +". Your grade: "+ nota +". You failed the course.";
+  }else{
+    return "Class average: "+ media +". Your grade: "+ nota +". You passed the course.";
+  }
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
